d8ad1f7b03e46df3877ff456b2419d5a7c51398e
Musab-23 <musabshaikh16@gmail.com>
2025-11-07 19:40:18 +0500

    working on js

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-// let marks=85;
-//     if(marks>90){
-//         console.log("Grade A");
-//     } else if(marks>75){
-//         console.log("Grade B");
-//     } else if(marks>60){
-//         console.log("Grade C");
-//     } else if(marks>50){
-//         console.log("Grade D");
-//     } else {
-//         console.log("Grade F");
-//     }  
+// // let marks=85;
+// //     if(marks>90){
+// //         console.log("Grade A");
+// //     } else if(marks>75){
+// //         console.log("Grade B");
+// //     } else if(marks>60){
+// //         console.log("Grade C");
+// //     } else if(marks>50){
+// //         console.log("Grade D");
+// //     } else {
+// //         console.log("Grade F");
+// //     }  
+
+// // let marks=prompt("Enter your marks:");
+// //     if(marks<0 || marks>100){
+// //        alert("Invalid marks entered");
+// //     } else if(marks>90){
+// //         alert(" you got Grade A");
+// //     } else if(marks>75){
+// //         alert("you got Grade B");
+// //     } else if(marks>60){
+// //         alert("you got Grade C");
+// //     } else if(marks>50){
+// //         alert("you got Grade D");
+// //     } else {
+// //         alert("you got Grade F");
+// //         alert("fail");
+// //     }
+// for(let i=1;i<=5;i++){
+//     console.log('count:',i);
+// }
+// // let marks=85;
+// //     if(marks>90){
+// //         console.log("Grade A");
+// //     } else if(marks>75){
+// //         console.log("Grade B");
+// //     } else if(marks>60){
+// //         console.log("Grade C");
+// //     } else if(marks>50){
+// //         console.log("Grade D");
+// //     } else {
+// //         console.log("Grade F");
+// //     }  
 
 // let marks=prompt("Enter your marks:");
 //     if(marks<0 || marks>100){
@@ -26,34 +57,17 @@
 //         alert("you got Grade F");
 //         alert("fail");
 //     }
-for(let i=1;i<=5;i++){
-    console.log('count:',i);
-}
-// let marks=85;
-//     if(marks>90){
-//         console.log("Grade A");
-//     } else if(marks>75){
-//         console.log("Grade B");
-//     } else if(marks>60){
-//         console.log("Grade C");
-//     } else if(marks>50){
-//         console.log("Grade D");
-//     } else {
-//         console.log("Grade F");
-//     }  
+// let count=1;
+// while(count>3){
+//     console.log(count);count++;
+// }
+// // let num=1;
+// // do{
+// //     console.log(num);
+// //     num++;
+// // }while(num<=3);
 
-let marks=prompt("Enter your marks:");
-    if(marks<0 || marks>100){
-       alert("Invalid marks entered");
-    } else if(marks>90){
-        alert(" you got Grade A");
-    } else if(marks>75){
-        alert("you got Grade B");
-    } else if(marks>60){
-        alert("you got Grade C");
-    } else if(marks>50){
-        alert("you got Grade D");
-    } else {
-        alert("you got Grade F");
-        alert("fail");
-    }
+for(let i=1;i<=10;i++){
+    if(i===5) continue;
+    console.log(i);
+}
