297f2e5fc9e2c96223be888de18753bd57ed82cf
Musab-23 <musabshaikh16@gmail.com>
2025-11-10 19:48:39 +0500

    feet:

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-// // let marks=85;
-// //     if(marks>90){
-// //         console.log("Grade A");
-// //     } else if(marks>75){
-// //         console.log("Grade B");
-// //     } else if(marks>60){
-// //         console.log("Grade C");
-// //     } else if(marks>50){
-// //         console.log("Grade D");
-// //     } else {
-// //         console.log("Grade F");
-// //     }  
+// // // let marks=85;
+// // //     if(marks>90){
+// // //         console.log("Grade A");
+// // //     } else if(marks>75){
+// // //         console.log("Grade B");
+// // //     } else if(marks>60){
+// // //         console.log("Grade C");
+// // //     } else if(marks>50){
+// // //         console.log("Grade D");
+// // //     } else {
+// // //         console.log("Grade F");
+// // //     }  
+
+// // // let marks=prompt("Enter your marks:");
+// // //     if(marks<0 || marks>100){
+// // //        alert("Invalid marks entered");
+// // //     } else if(marks>90){
+// // //         alert(" you got Grade A");
+// // //     } else if(marks>75){
+// // //         alert("you got Grade B");
+// // //     } else if(marks>60){
+// // //         alert("you got Grade C");
+// // //     } else if(marks>50){
+// // //         alert("you got Grade D");
+// // //     } else {
+// // //         alert("you got Grade F");
+// // //         alert("fail");
+// // //     }
+// // for(let i=1;i<=5;i++){
+// //     console.log('count:',i);
+// // }
+// // // let marks=85;
+// // //     if(marks>90){
+// // //         console.log("Grade A");
+// // //     } else if(marks>75){
+// // //         console.log("Grade B");
+// // //     } else if(marks>60){
+// // //         console.log("Grade C");
+// // //     } else if(marks>50){
+// // //         console.log("Grade D");
+// // //     } else {
+// // //         console.log("Grade F");
+// // //     }  
 
 // // let marks=prompt("Enter your marks:");
 // //     if(marks<0 || marks>100){
@@ -26,80 +57,57 @@
 // //         alert("you got Grade F");
 // //         alert("fail");
 // //     }
-// for(let i=1;i<=5;i++){
-//     console.log('count:',i);
+// // let count=1;
+// // while(count>3){
+// //     console.log(count);count++;
+// // }
+// // // let num=1;
+// // // do{
+// // //     console.log(num);
+// // //     num++;
+// // // }while(num<=3);
+
+// // for(let i=1;i<=10;i++){
+// //     if(i===5) continue;
+// //     console.log(i);
+// // }
+// // function greet() {
+// //     console.log("Hello");
+// // }
+
+// // greet();
+// // 
+
+// function add(a,b){
+//     return a+b;
+
 // }
-// // let marks=85;
-// //     if(marks>90){
-// //         console.log("Grade A");
-// //     } else if(marks>75){
-// //         console.log("Grade B");
-// //     } else if(marks>60){
-// //         console.log("Grade C");
-// //     } else if(marks>50){
-// //         console.log("Grade D");
-// //     } else {
-// //         console.log("Grade F");
-// //     }  
+// let sum = add(5,3);
+// console.log(sum);
 
-// let marks=prompt("Enter your marks:");
-//     if(marks<0 || marks>100){
-//        alert("Invalid marks entered");
-//     } else if(marks>90){
-//         alert(" you got Grade A");
-//     } else if(marks>75){
-//         alert("you got Grade B");
-//     } else if(marks>60){
-//         alert("you got Grade C");
-//     } else if(marks>50){
-//         alert("you got Grade D");
-//     } else {
-//         alert("you got Grade F");
-//         alert("fail");
-//     }
-// let count=1;
-// while(count>3){
-//     console.log(count);count++;
+// function multiplication(a,b){
+//     return a*b;
+
 // }
-// // let num=1;
-// // do{
-// //     console.log(num);
-// //     num++;
-// // }while(num<=3);
+// let multiple = multiplication(5,3);
+// console.log(multiple);
 
-// for(let i=1;i<=10;i++){
-//     if(i===5) continue;
-//     console.log(i);
+// function division(a,b){
+//     return a/b;
+
 // }
-// function greet() {
-//     console.log("Hello");
+// let divide = division(5,3);
+// console.log(divide);
+
+// function subtract(a,b){
+//     return a-b;
+
 // }
+// let subtraction = subtract(5,3);
+// console.log(subtraction);
+const add = (a,b) => a+b;
+console.log(add(5,3));
 
-// greet();
-function add(a,b){
-    return a+b;
+const greet = (name) => `${name}`;
+console.log(greet('Musab'));
 
-}
-let sum = add(5,3);
-console.log(sum);
-
-function multiplication(a,b){
-    return a*b;
-
-}
-let multiple = multiplication(5,3);
-console.log(multiple);
-
-function division(a,b){
-    return a/b;
-
-}
-let divide = division(5,3);
-console.log(divide);
-
-function subtract(a,b){
-    return a-b;
-
-}
-let subtraction = subtract(5,3);
-console.log(subtraction);
